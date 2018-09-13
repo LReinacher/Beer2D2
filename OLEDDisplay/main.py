9bd8632c1793082f1cd23abd3a9ff95ef9271fb6
lreinacher <lion.reinacher@gmail.com>
2018-09-13 11:09:26 +0200

    editing display

--- OLEDDisplay/main.py
+++ OLEDDisplay/main.py
@@ -7,57 +7,77 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+import glob_vars
+
 import subprocess
 
-RST = None
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+def main():
+    RST = None
+    DC = 23
+    SPI_PORT = 0
+    SPI_DEVICE = 0
 
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-disp.begin()
+    disp.begin()
 
-disp.clear()
-disp.display()
+    disp.clear()
+    disp.display()
 
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
 
-draw = ImageDraw.Draw(image)
-
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-padding = -2
-top = padding
-bottom = height-padding
-x = 0
-
-font = ImageFont.load_default()
-
-while True:
+    draw = ImageDraw.Draw(image)
 
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+    padding = -2
+    top = padding
+    bottom = height-padding
+    x = 0
 
-    # Write two lines of text.
+    font = ImageFont.load_default()
 
-    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+    while True:
 
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(.1)
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        cmd = "iwgetid -r"
+        SSID = subprocess.check_output(cmd, shell=True)
+        cmd = "hostname -I | cut -d\' \' -f1"
+        IP = subprocess.check_output(cmd, shell = True )
+
+        if glob_vars.remote_system_motor_override:
+            mode = "Remote"
+        else:
+            mode = "LineTracking"
+
+        m1, m2 = glob_vars.current_motor_state
+        if m1 == "Forwards":
+            m1 = "Fwd"
+        elif m1 == "Backwards":
+            m1 = "Bwd"
+        else:
+            m1 = "Stp"
+
+        if m2 == "Forwards":
+            m2 = "Fwd"
+        elif m2 == "Backwards":
+            m2 = "Bwd"
+        else:
+            m2 = "Stp"
+
+        # Write two lines of text.
+
+        draw.text((x, top),       "SSID: " + str(SSID.decode('UTF-8')),  font=font, fill=255)
+        draw.text((x, top+8),     "IP: " + str(IP.decode('UTF-8')), font=font, fill=255)
+        draw.text((x, top+20),    "Mode: " + str(mode),  font=font, fill=255)
+        draw.text((x, top+28),    "Dest: " + str(glob_vars.current_destination),  font=font, fill=255)
+        draw.text((x, top + 38), "Motors: " + str(m1) + " - " + str(m2), font=font, fill=255)
+
+        # Display image.
+        disp.image(image)
+        disp.display()
+        time.sleep(.1)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import RestAPI.main as RestAPI
 import SlackBot.main as SlackBot
 import MotorControl.util as MotorControl
 import glob_vars
-import WebcamHandler.line_qr_tracker as WebCamHandler
+import settings
+#import WebcamHandler.line_qr_tracker as WebCamHandler
+import OLEDDisplay.main as OLEDDisplay
 
 if __name__ == "__main__":
     glob_vars.motorControlInstance = MotorControl.MotorControl()
@@ -15,6 +17,10 @@ if __name__ == "__main__":
 
     api_thread = Thread(target=RestAPI.start, args=(), name="API", daemon=False)
     api_thread.start()
+
+    if settings.localhost is False:
+        display_thread = Thread(target=OLEDDisplay.main, args=(), name="Display", daemon=False)
+        display_thread.start()
 
     #WebCam_Thread = Thread(target=WebCamHandler.main, args=(), name="WebCamHandler", daemon=False)
     #WebCam_Thread.start()
